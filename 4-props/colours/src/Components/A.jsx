8c3aacb5d188ea57461bbabe2de027ea038453b9
jibanranjan <jdebata427@gmail.com>
2023-08-11 20:31:44 +0530

    initial commit

---
+++ 4-props/colours/src/Components/A.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './A.css'
+function A() {
+  return (
+    <div className='card'>
+
+         <div className="cardTop" >
+
+         </div>
+
+         <div className="cardButtom">
+
+         </div>
+
+    </div>
+  )
+}
+
+export default A
